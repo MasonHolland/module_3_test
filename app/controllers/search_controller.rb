23a387441141be2b65218fabd71fa160667b9ab5
MasonHolland <masontholland@gmail.com>
2017-09-13 10:52:15 -0600

    api call working in controller. refactoring to service now

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,12 @@
+class SearchController < ApplicationController
+
+  def index
+    conn = Faraday.new("https://api.bestbuy.com")
+    response = conn.get do |req|
+      req.url "/v1/stores(area(80202,25))?format=json&show=longName,city,storeType,phone,distance&pageSize=10&apiKey=#{ENV['bb_api_key']}"
+    end
+    @stores_presenter = JSON.parse(response.body)
+    binding.pry
+  end
+
+end
