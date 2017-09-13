4de163fc4d5f6633f2fdec149ace4b7191adeb4a
MasonHolland <masontholland@gmail.com>
2017-09-13 11:00:59 -0600

    service making requests successfully

---
+++ app/services/best_buy_service.rb
@@ -0,0 +1,23 @@
+class BestBuyService
+  def initialize
+    @conn = Faraday.new("https://api.bestbuy.com")
+  end
+
+  def self.store_search(zip)
+    new.store_search(zip)
+  end
+
+  def store_search(zip)
+    response = conn.get do |req|
+      req.url   "/v1/stores(area(#{zip},25))"
+      req.params['format'] = 'json'
+      req.params['show'] = 'longName,city,storeType,phone,distance'
+      req.params['pageSize'] = '10'
+      req.params['apiKey'] = ENV['bb_api_key']
+    end
+    JSON.parse(response.body)
+  end
+
+  private
+    attr_reader :conn
+end
